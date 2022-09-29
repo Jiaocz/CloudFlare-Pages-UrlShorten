b49895c58bd0462eb29dfb43d3f85e36f7b00466
橙梓 <me@orangii.cn>
2022-09-29 14:08:47 +0800

    Basic index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    content: ['./dist/**/*.html'],
-  },
-  content: [],
+  content: ['./dist/**/*.html'],
   theme: {
     extend: {},
   },
